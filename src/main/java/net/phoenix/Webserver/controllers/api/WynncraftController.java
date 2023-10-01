f65e9f31735fc05dae02b27b32c69c0565f02be0
PhoenixOrigin <phoenixorigin171@gmail.com>
2023-10-01 22:09:56 +0100

    maybe work

--- src/main/java/net/phoenix/Webserver/controllers/api/WynncraftController.java
+++ src/main/java/net/phoenix/Webserver/controllers/api/WynncraftController.java
@@ -15,9 +15,9 @@ public class WynncraftController {
     @GetMapping("/playtime")
     public ResponseEntity<String> getPlaytime(@RequestParam(name = "username") String username, @RequestParam(name = "from", required = false) String from) {
         if (from == null) {
-            return ResponseEntity.ok(String.format("{\"playtime\": %d)", DatabaseHandler.get_playtime(username)));
+            return ResponseEntity.ok(String.format("{\"playtime\": %d}", DatabaseHandler.get_playtime(username)));
         } else {
-            return ResponseEntity.ok(String.format("{\"playtime\": %d)", DatabaseHandler.get_playtime(username, Timestamp.from(Instant.ofEpochMilli(Long.parseLong(from))))));
+            return ResponseEntity.ok(String.format("{\"playtime\": %d}", DatabaseHandler.get_playtime(username, Timestamp.from(Instant.ofEpochMilli(Long.parseLong(from))))));
         }
     }
 
