877913419f0051acd33938e371454e67f49f7f44
PhoenixOrigin <phoenixorigin171@gmail.com>
2023-10-01 21:09:02 +0100

    attempt to use reverse proxy instead

--- src/main/java/net/phoenix/Webserver/WebserverApplication.java
+++ src/main/java/net/phoenix/Webserver/WebserverApplication.java
@@ -10,7 +10,6 @@ public class WebserverApplication {
 
     public static void main(String[] args) {
         SpringApplication application = new SpringApplication(WebserverApplication.class);
-        application.setAdditionalProfiles("ssl");
         application.run(args);
         //DatabaseHandler.init();
     }
