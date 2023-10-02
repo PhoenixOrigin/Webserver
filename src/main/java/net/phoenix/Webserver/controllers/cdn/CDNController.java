8f3a049acc207dd238e2d518622a853c45a21018
PhoenixOrigin <phoenixorigin171@gmail.com>
2023-10-02 21:42:59 +0100

    cdn exists now

---
+++ src/main/java/net/phoenix/Webserver/controllers/cdn/CDNController.java
@@ -0,0 +1,45 @@
+package net.phoenix.Webserver.controllers.cdn;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@RestController
+public class CDNController {
+
+    @Value("${cdn.content-path}")
+    private String externalCdnPath;
+
+    @GetMapping("/cdn/{resourceName:.+}")
+    public ResponseEntity<Resource> serveCDNResource(@PathVariable String resourceName) {
+        try {
+            Path filePath = Paths.get(externalCdnPath).resolve(resourceName);
+            Resource resource = new UrlResource(filePath.toUri());
+
+            if (resource.exists() && resource.isReadable()) {
+                HttpHeaders headers = new HttpHeaders();
+                headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+                headers.setContentDispositionFormData("attachment", resourceName);
+                return ResponseEntity.ok()
+                        .headers(headers)
+                        .body(resource);
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        } catch (IOException e) {
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+}
