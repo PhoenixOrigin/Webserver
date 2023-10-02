6d5daca68635d19d2e9277493268277f56d2ae96
PhoenixOrigin <phoenixorigin171@gmail.com>
2023-10-02 06:39:22 +0100

    changes

--- src/main/java/net/phoenix/Webserver/tasks/Tasks.java
+++ src/main/java/net/phoenix/Webserver/tasks/Tasks.java
@@ -1,6 +1,7 @@
 package net.phoenix.Webserver.tasks;
 
 import net.phoenix.Webserver.handlers.api.HypixelHandler;
+import net.phoenix.Webserver.trackers.WynncraftPlaytime;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -9,7 +10,7 @@ public class Tasks {
 
     @Scheduled(fixedRate = 300000)
     public static void recordWynnPlaytime() {
-        //WynncraftPlaytime.recordPlaytime();
+        WynncraftPlaytime.recordPlaytime();
     }
 
     @Scheduled(fixedRate = 300000)
