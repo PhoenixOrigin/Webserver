162559d5b581139f8eb4656c8448a2d29d3b88c4
PhoenixOrigin <phoenixorigin171@gmail.com>
2023-10-02 18:14:33 +0100

    idk why pt not working

--- src/main/java/net/phoenix/Webserver/trackers/WynncraftPlaytime.java
+++ src/main/java/net/phoenix/Webserver/trackers/WynncraftPlaytime.java
@@ -3,16 +3,24 @@ package net.phoenix.Webserver.trackers;
 import net.phoenix.Webserver.database.DatabaseHandler;
 import net.phoenix.Webserver.handlers.api.WynncraftHandler;
 import net.phoenix.Webserver.utils.Utilities;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.util.List;
 
 public class WynncraftPlaytime {
 
+    private static Logger logger = LoggerFactory.getLogger(WynncraftPlaytime.class);
+
     public static void recordPlaytime() {
         try {
-            WynncraftHandler.onlinePlayers().forEach((player) -> {
+            logger.info("Attempting to log playtime");
+            List<String> onlinePlayers = WynncraftHandler.onlinePlayers();
+            onlinePlayers.forEach((player) -> {
                 DatabaseHandler.record_playtime(Utilities.toUUID(player));
             });
+            logger.info("Recorded playtime for " + onlinePlayers.toString());
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
