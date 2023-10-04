38b11add3bc1ccf51c621fdca2b29d2cb6c34ce4
PhoenixOrigin <phoenixorigin171@gmail.com>
2023-10-04 06:49:48 +0100

    cdn exists now

--- src/main/java/net/phoenix/Webserver/controllers/redirect/RedirectController.java
+++ src/main/java/net/phoenix/Webserver/controllers/redirect/RedirectController.java
@@ -1,6 +1,5 @@
 package net.phoenix.Webserver.controllers.redirect;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
