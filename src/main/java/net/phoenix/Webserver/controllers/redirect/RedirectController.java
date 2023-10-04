01cb80bba6fea424ae265c7a2e20d0dba7dab528
PhoenixOrigin <phoenixorigin171@gmail.com>
2023-10-04 06:49:32 +0100

    cdn exists now

---
+++ src/main/java/net/phoenix/Webserver/controllers/redirect/RedirectController.java
@@ -0,0 +1,18 @@
+package net.phoenix.Webserver.controllers.redirect;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
+@RestController
+public class RedirectController {
+
+    @RequestMapping("/hax")
+    public RedirectView localRedirect() {
+        RedirectView redirectView = new RedirectView();
+        redirectView.setUrl("https://discord.gg/eH5ynaQ");
+        return redirectView;
+    }
+
+}
